000a1a1b6a8ff9ff30d364f0014f3976e34bbce4
Edgar K <alpha9777@gmail.com>
2023-06-19 13:16:08 +0300

    changed nextauth variable

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions = {
-  secret: process.env.NextAuth_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: 'Credentials',
